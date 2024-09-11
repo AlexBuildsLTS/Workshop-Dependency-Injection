a1245ef198159c42891e3a1b8f65c9aee3011643
Alex Youssef <Alex.youssef@live.com>
2024-09-11 10:12:44 +0200

    Implement StudentManagement service with console input handling

--- src/main/java/se/alex/lexicon/management/StudentManagement.java
+++ src/main/java/se/alex/lexicon/management/StudentManagement.java
@@ -1,4 +1,12 @@
 package se.alex.lexicon.management;
 
-public class StudentManagement {
+import se.alex.lexicon.models.Student;
+import java.util.List;
+
+public interface StudentManagement {
+    Student create();
+    Student save(Student student);
+    Student find(int id);
+    List<Student> findAll();
+    void delete(int id);
 }
--- src/main/java/se/alex/lexicon/management/StudentManagementConsoleImpl.java
+++ src/main/java/se/alex/lexicon/management/StudentManagementConsoleImpl.java
@@ -1,4 +1,52 @@
 package se.alex.lexicon.management;
 
-public class StudentManagementConsoleImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import se.alex.lexicon.data_access.StudentDao;
+import se.alex.lexicon.models.Student;
+import se.alex.lexicon.services.UserInputService;
+
+import java.util.List;
+
+@Component
+public class StudentManagementConsoleImpl implements StudentManagement {
+
+    private final UserInputService userInputService;
+    private final StudentDao studentDao;
+
+    @Autowired
+    public StudentManagementConsoleImpl(UserInputService userInputService, StudentDao studentDao) {
+        this.userInputService = userInputService;
+        this.studentDao = studentDao;
+    }
+
+    @Override
+    public Student create() {
+        Student student = new Student();
+        System.out.println("Enter student ID:");
+        student.setId(userInputService.getInt());
+        System.out.println("Enter student name:");
+        student.setName(userInputService.getString());
+        return student;
+    }
+
+    @Override
+    public Student save(Student student) {
+        return studentDao.save(student);
+    }
+
+    @Override
+    public Student find(int id) {
+        return studentDao.find(id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return studentDao.findAll();
+    }
+
+    @Override
+    public void delete(int id) {
+        studentDao.delete(id);
+    }
 }
